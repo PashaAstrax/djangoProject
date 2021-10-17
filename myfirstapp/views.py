56bdcc02f733c253b0db0c1e2d99a0abf8e7bc30
PashaAstrax <pashaastrax@gmail.com>
2021-10-17 13:31:57 +0300

    init

--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
-def multi(request, num1, action, num2):
-    global res
+def multi(request, num1, action, num2, num3):
+
     if action == "*":
-        res = int(num1 * num2)
-    elif action == "div":
-        res = int(num1 / num2)
-    elif action == "+":
-        res = int(num1 + num2)
-    elif action == "-":
-        res = int(num1 - num2)
-    return render(request, "Multi_table.html", {"num1": num1, "action": action, "num2": num2, "res": res})
+        res = f"{num1} * {num2} = {num3}"
+        return res
+
+    # elif action == "div":
+    #     res = int(num1 / num2)
+    # elif action == "+":
+    #     res = int(num1 + num2)
+    # elif action == "-":
+    #     res = int(num1 - num2)
+    return render(request, "Multi_table.html", {"num1": num1, "action": action, "num2": num2, "num3": num3})
