f3b45bc137fb4566ccb75f0cdd2748c55cb28536
PashaAstrax <pashaastrax@gmail.com>
2021-10-19 15:27:30 +0300

    init

--- settings/urls.py
+++ settings/urls.py
@@ -1,4 +1,4 @@
-"""djangoProject URL Configuration
+"""settings URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myfirstapp.views import ComputerListCreateView, ComputerRetrieveUpdateDestroyView
+from cars.views import ComputerListCreateView, ComputerRetrieveUpdateDestroyView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
